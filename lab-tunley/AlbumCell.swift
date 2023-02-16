4c9232e5593eac10134d05a5bf62579b3b014d6a
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-15 22:52:44 -0800

    Setup collection view cell

---
+++ lab-tunley/AlbumCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by Stephanie Hernandez on 2/15/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+}
